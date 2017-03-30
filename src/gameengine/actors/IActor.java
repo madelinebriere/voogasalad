6166e34b3d02231945c0bc279c75140fe93caa10
SARAH ZHOU <sz107@duke.edu>
2017-03-30 15:21:01 -0400

    added public methods for IGameObject, IActor, and ISmartActor

--- src/gameengine/actors/IActor.java
+++ src/gameengine/actors/IActor.java
@@ -7,4 +7,12 @@ public interface IActor {
 	public ActorResult act();
 	
 	public boolean isActive();
+	
+	public double getHealth();
+	
+	public void setHealth(double health);
+	
+	public void dies();
+	
+	public boolean hasDied();
 }
