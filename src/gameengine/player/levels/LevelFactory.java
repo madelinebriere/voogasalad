19b2377bc2127aea172f3464f9a13302ecbd804f
Madeline Briere <mfb33@duke.edu>
2017-04-02 23:13:22 -0400

    Delegating tasks
    Former-commit-id: 191d35d0e6cff3801b0020e923c43ab00e10fb6c

--- src/gameengine/player/levels/LevelFactory.java
+++ src/gameengine/player/levels/LevelFactory.java
@@ -3,6 +3,12 @@ package gameengine.player.levels;
 import gamedata.GameData;
 import gameengine.controllers.LevelController;
 
+/**
+ * 
+ * @author maddiebriere
+ *
+ */
+
 public interface LevelFactory {
 
 	public LevelController buildLevel(GameData current);
