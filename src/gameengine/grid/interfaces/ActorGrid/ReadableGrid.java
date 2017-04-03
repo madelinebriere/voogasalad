8e931586fcccbf27fef1d30fc667a0f49b011eba
Gideon Pfeffer <gap16@duke.edu>
2017-04-03 19:50:50 -0400

    implemented all grid methods except for the projectiles
    Former-commit-id: 280b28f7510d7be265c8aa5610300795a6337a4a

--- src/gameengine/grid/interfaces/ActorGrid/ReadableGrid.java
+++ src/gameengine/grid/interfaces/ActorGrid/ReadableGrid.java
@@ -1,15 +1,14 @@
 package gameengine.grid.interfaces.ActorGrid;
 
 import java.util.Collection;
-import java.util.List;
 
 import gameengine.actors.management.Actor;
 import gameengine.grid.interfaces.Identifiers.Grid2D;
 
 public interface ReadableGrid{
 	
-	List<Actor<? extends ReadableGrid>> getInRadius(double x, double y, double radius);
-	Grid2D getLocationOf(Actor<? extends ReadableGrid> t);
+	Collection<Grid2D> getEnemiesInRadius(double x, double y, double radius);
+	Grid2D getLocationOf(double id);
 	Collection<Grid2D> getEnemyLocations();
 	Collection<Grid2D> getTowerLocations();
 	Collection<Grid2D> getBaseLocations();
