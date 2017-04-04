e1dd4427fee4ba7c04eba75d477ec11d0f5afe5e
Madeline Briere <mfb33@duke.edu>
2017-04-04 19:12:03 -0400

    Modifications to LevelData/Controller/Factory
    Former-commit-id: f0b8e3df0350c524821e30dc55561945c531858b

--- src/gamedata/LevelData.java
+++ src/gamedata/LevelData.java
@@ -17,16 +17,19 @@ import gamedata.composition.MoveWithDestinationData;
 public class LevelData {
 	private double myMaxX;
 	private double myMaxY;
-	private List<List<Data>> pieces;
-	//TODO: add other variables
+	private List<List<Data>> shots;
+	private List<List<Data>> towers;
+	private List<List<Data>> troops;
+	private List<List<Data>> bases;
 
 	public LevelData(double maxX, double maxY){
 		myMaxX = maxX;
 		myMaxY = maxY;
 	}
 	
+	//TODO: Implement
 	//TODO: Limit input based on types of parameters required for type
-	//TODO: Get rid of dependnecy on enum actor type to match method header 
+	//TODO: Get rid of dependency on enum actor type to match method header 
 	public void addShot(ActorData actor, 
 			MoveWithDestinationData move, HealthData data, 
 			Data...datas){
@@ -48,9 +51,45 @@ public class LevelData {
 		
 	}
 	
-	public void add(Data...datas){
+	public void add(List<List<Data>> pieces, Data...datas){
 		pieces.add(Arrays.asList(datas));
 	}
+
+	public double getMyMaxX() {
+		return myMaxX;
+	}
+
+	public void setMyMaxX(double myMaxX) {
+		this.myMaxX = myMaxX;
+	}
+
+	public double getMyMaxY() {
+		return myMaxY;
+	}
+
+	public void setMyMaxY(double myMaxY) {
+		this.myMaxY = myMaxY;
+	}
+
+	public List<List<Data>> getShots() {
+		return shots;
+	}
+
+	public List<List<Data>> getTowers() {
+		return towers;
+	}
+
+	public List<List<Data>> getTroops() {
+		return troops;
+	}
+
+	public List<List<Data>> getBases() {
+		return bases;
+	}
+	
+	
+
+	
 	
 	
 }
