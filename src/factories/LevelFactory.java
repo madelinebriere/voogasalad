a4bc26e92f51b12996c5902f752ab73fcc78f7d7
Madeline Briere <mfb33@duke.edu>
2017-04-03 23:32:21 -0400

    Additions for the night
    Former-commit-id: 0aac1feb569ca1298078dd315cc1c4f043d8d9c2

--- src/factories/LevelFactory.java
+++ src/factories/LevelFactory.java
@@ -1,7 +1,13 @@
 package factories;
 
+import java.util.Map;
+
+import gamedata.ActorData;
 import gamedata.GameData;
+import gamedata.GridData;
+import gamedata.LevelData;
 import gameengine.controllers.LevelController;
+import gameengine.grid.classes.Coordinates;
 
 /**
  * Generates a new level from the current game data
@@ -10,7 +16,15 @@ import gameengine.controllers.LevelController;
  *
  */
 
-public interface LevelFactory {
+public class LevelFactory {
 
-	public LevelController buildLevel(GameData current, int level);
+	public LevelController buildLevel(GameData current, int level){
+		LevelData curr = current.getLevel(level);
+		GridData data = curr.getMyGrid();
+		//TODO: Make better data structure for this
+		Map<ActorData, Coordinates> myGrid = data.getActorMap();
+		
+		//TODO: complete
+		return null;
+	}
 }
