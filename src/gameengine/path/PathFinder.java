3203fc5e543b13235bcf303d85610905a49ba830
ANH TRINH <ANT19@duke.edu>
2017-04-03 22:47:53 -0400

    Delete WeakEnemy, Grid2D, change name from Dimensions to Coordinate, add asString method to Coordinate
    Former-commit-id: a068e016e18514a5a991e25f0a69070f0ef7c673

--- src/gameengine/path/PathFinder.java
+++ src/gameengine/path/PathFinder.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import gameengine.grid.classes.Grid2D;
+import gameengine.grid.classes.Coordinates;
 
 /**
  * return a path as a list of all coordinates for an enemy to go for each step. It should know the path index and the
@@ -16,16 +16,16 @@ import gameengine.grid.classes.Grid2D;
  */
 public class PathFinder  {
 	
-	private Map<Integer,List<Grid2D>> myPaths; 
+	private Map<Integer,List<Coordinates>> myPaths; 
 	
-	public PathFinder(Map<Integer,List<Grid2D>> allPaths ){
+	public PathFinder(Map<Integer,List<Coordinates>> allPaths ){
 		myPaths = allPaths;	
 	}
 	
-	public List<Grid2D> getPathCoordinates(Integer pathNo, double increment){
+	public List<Coordinates> getPathCoordinates(Integer pathNo, double increment){
 		
-		List<Grid2D> coordinatesToReturn = new ArrayList<Grid2D>(); 
-		List<Grid2D> pathChosen = myPaths.get(pathNo); 
+		List<Coordinates> coordinatesToReturn = new ArrayList<Coordinates>(); 
+		List<Coordinates> pathChosen = myPaths.get(pathNo); 
 		
 		
 		for (int i = 0; i< pathChosen.size()-1; i++ ){
@@ -35,14 +35,14 @@ public class PathFinder  {
 			System.out.println("current list has");
 			coordinatesToReturn.forEach(e -> System.out.println(e.asString()));
 		}
-		coordinatesToReturn.add(new Grid2D(pathChosen.get(pathChosen.size()-1))); // for the ending point
+		coordinatesToReturn.add(new Coordinates(pathChosen.get(pathChosen.size()-1))); // for the ending point
 		return coordinatesToReturn;
 	}
 	
 
 	
-	private List<Grid2D> getIncrementPoints(Grid2D start,Grid2D end, double increment){
-		List<Grid2D> result = new ArrayList<Grid2D>();
+	private List<Coordinates> getIncrementPoints(Coordinates start,Coordinates end, double increment){
+		List<Coordinates> result = new ArrayList<Coordinates>();
 		double distance = getDistance(start, end); 
 		System.out.println("distance between " + start.asString() + " and " + end.asString() + " is: " + distance);
 		
@@ -55,17 +55,17 @@ public class PathFinder  {
 		for (int i = 0; i <= numIncrements; i++){
 			double newX = start.getX() + increment* i * Math.cos(angle);
 			double newY = start.getY() + increment* i * Math.sin(angle);
-			result.add(new Grid2D(newX,newY));
+			result.add(new Coordinates(newX,newY));
 		}
 		return result; 
 	}
 	
-	private double getDistance(Grid2D point1,Grid2D point2){
+	private double getDistance(Coordinates point1,Coordinates point2){
 		return Math.sqrt(Math.pow(point2.getX() - point1.getX(),2) 
 				+ Math.pow(point2.getY() - point1.getY(),2));
 	}
 	
-	private double getAngle(Grid2D point1,Grid2D point2){
+	private double getAngle(Coordinates point1,Coordinates point2){
 		return Math.atan2(point2.getY() - point1.getY(), point2.getX() - point1.getX());
 	}
 	
@@ -74,19 +74,19 @@ public class PathFinder  {
 	
 	
 	public static void main(String[] args){
-		Map<Integer,List<Grid2D>> map = new HashMap<Integer, List<Grid2D>>();
-		List<Grid2D> wayPoints = new ArrayList<Grid2D>(); 
-		wayPoints.add(new Grid2D(0,0));
-		wayPoints.add(new Grid2D(5,0));
-		wayPoints.add(new Grid2D(10,0));
-		wayPoints.add(new Grid2D(13,0));
+		Map<Integer,List<Coordinates>> map = new HashMap<Integer, List<Coordinates>>();
+		List<Coordinates> wayPoints = new ArrayList<Coordinates>(); 
+		wayPoints.add(new Coordinates(0,0));
+		wayPoints.add(new Coordinates(5,0));
+		wayPoints.add(new Coordinates(10,0));
+		wayPoints.add(new Coordinates(13,0));
 		
 		
 		
 		map.put(0, wayPoints);
 		
 		PathFinder test = new PathFinder(map); 
-		List<Grid2D> path = test.getPathCoordinates(0,2);
+		List<Coordinates> path = test.getPathCoordinates(0,2);
 		
 		System.out.println("**********");
 		
