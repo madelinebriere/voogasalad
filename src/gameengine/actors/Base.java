2d9615b0b4bc9d51dce7114918e64373bf4d1dd1
Moses Wayne <msw38@duke.edu>
2017-04-05 04:03:22 -0400

    Fixed almost all generic problems: involved changing numerous actor classes as well as the ActorGrid class. NOTE: ActorGrid should not have the step() function in it, this will cause problems later if used. The calling of the act API should only be invoked by the GameController.
    Former-commit-id: 5a291a7c9e1a7fba3e2afc3fc4f5a03a913d5460

--- src/gameengine/actors/Base.java
+++ src/gameengine/actors/Base.java
@@ -4,10 +4,24 @@ import java.util.List;
 
 import gameengine.actors.properties.HealthProperty;
 import gameengine.actors.properties.IActProperty;
+import gameengine.grid.interfaces.ActorGrid.MasterGrid;
+import gameengine.grid.interfaces.ActorGrid.ReadableGrid;
 
-public abstract class Base extends AbstractActor{
+public class Base<G extends ReadableGrid> extends AbstractActor<G>{
 	
-	public Base(HealthProperty h, List<IActProperty> properties){
-		//TODO: Complete
+	public Base(Integer id, HealthProperty h, List<IActProperty> properties){
+		super(id, h);
+	}
+
+	@Override
+	public void act(G grid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applyDamage(double health) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/gameengine/actors/Troop.java
+++ src/gameengine/actors/Troop.java
@@ -1,16 +1,49 @@
 package gameengine.actors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gameengine.actors.properties.HealthProperty;
 import gameengine.actors.properties.IActProperty;
+import gameengine.grid.interfaces.ActorGrid.MasterGrid;
 import gameengine.grid.interfaces.ActorGrid.ReadableGrid;
 
-public abstract class Troop extends AbstractActor {
+public class Troop<G extends ReadableGrid> extends AbstractActor<G> {
 
-	public Troop(HealthProperty h, List<IActProperty<ReadableGrid>> properties){	
+	private List<IActProperty<G>> myProperties; 
+	private int myID;
+	
+	public Troop(Integer id, HealthProperty h, IActProperty<G>... properties) {
+		super(id, h);
+		myProperties = new ArrayList<>();
+		for (IActProperty<G> p:properties) {
+			myProperties.add(p);
+		}
+	}
+
+
+
+	@Override
+	public void act(G grid) {
+		myProperties.forEach(e -> e.action(grid,myID));
+	}
+	
+	
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void applyDamage(double health) {
+		// TODO Auto-generated method stub
 		
 	}
 
+
+
+
+	
 }
 
