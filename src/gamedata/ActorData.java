e9b5522a28cc05c2f1fd9a5fe82bde35fa5ba7ff
Madeline Briere <mfb33@duke.edu>
2017-04-05 12:07:18 -0400

    Documentation and addition of frontend methods for communication
    Former-commit-id: 245586e4599e3cb1b96202aeff65105135ba332b

--- src/gamedata/ActorData.java
+++ src/gamedata/ActorData.java
@@ -8,12 +8,34 @@ import gamedata.composition.Data;
 import gamedata.composition.HealthData;
 import gamedata.composition.LimitedHealthData;
 
+/**
+ * Information required to construct a certain, 
+ * user-created Actor. Holds a name (e.g., "MyCoolTower")
+ * and a List of Data objects that represent the composition
+ * of the Actor. This is like a blueprint for the factory --
+ * when the user places one of these actors on the grid, 
+ * the backend can take all of the data objects and convert
+ * them to properties to create an Actor equivalent to the
+ * ActorData.
+ * 
+ * @author maddiebriere
+ *
+ */
+
 public class ActorData {
 	private final static double HEALTH = 100;
 	
 	private List<Data> myData;
 	private String name;
 	
+	/**
+	 * Easy implementation example:
+	 * 
+	 * ActorData myActor = new ActorData 
+	 * 		(new BasicData("Bob", images/bob));
+	 * 
+	 * @param data BasicData object with name and imagepath
+	 */
 	public ActorData(BasicData data){
 		this(data, new LimitedHealthData(HEALTH));
 	}
--- src/gamedata/LevelData.java
+++ src/gamedata/LevelData.java
@@ -3,8 +3,15 @@ package gamedata;
 import java.util.Map;
 
 /**
- * holds all the data that is encoded for a level
- * such as enemies, towers, and map
+ * Holds all the data that is encoded for a level
+ * such as:
+ * 1) Enemies
+ * 2) Preferences (e.g., waves of attack)
+ * 3) Difficulty
+ * 4) Duration of enemy attack
+ * 5) Health, attack and speed multipliers
+ * (more of each with higher levels)
+ * 
  * @author Maddie
  *
  */
@@ -22,8 +29,63 @@ public class LevelData {
 	public LevelData(Map<ActorData, Integer> troops){
 		//TODO: Implement constructors
 	}
-	
 
+	public PreferencesData getMyPreferences() {
+		return myPreferences;
+	}
+
+	public void setMyPreferences(PreferencesData myPreferences) {
+		this.myPreferences = myPreferences;
+	}
+
+	public Map<ActorData, Integer> getTroops() {
+		return troops;
+	}
+
+	public void setTroops(Map<ActorData, Integer> troops) {
+		this.troops = troops;
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+
+	public double getHealthMultiplier() {
+		return healthMultiplier;
+	}
+
+	public void setHealthMultiplier(double healthMultiplier) {
+		this.healthMultiplier = healthMultiplier;
+	}
+
+	public double getAttackMultiplier() {
+		return attackMultiplier;
+	}
+
+	public void setAttackMultiplier(double attackMultiplier) {
+		this.attackMultiplier = attackMultiplier;
+	}
+
+	public double getSpeedMultiplier() {
+		return speedMultiplier;
+	}
+
+	public void setSpeedMultiplier(double speedMultiplier) {
+		this.speedMultiplier = speedMultiplier;
+	}
+	
 	
 	
 }
