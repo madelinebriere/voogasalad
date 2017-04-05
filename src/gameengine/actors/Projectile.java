37c841bd2eb30f66cd43e9ef090ee802b9648109
Hari <hari.rajan@duke.edu>
2017-04-04 22:00:28 -0400

    minor changes
    Former-commit-id: 3a6c213d3ff055fcf0127e4afc6172237775ec53

--- src/gameengine/actors/Projectile.java
+++ src/gameengine/actors/Projectile.java
@@ -2,12 +2,13 @@ package gameengine.actors;
 
 import java.util.List;
 
+import gameengine.actors.management.Actor;
 import gameengine.actors.properties.HealthProperty;
 import gameengine.actors.properties.IActProperty;
 import gameengine.actors.properties.MoveWithDestinationProperty;
 import gameengine.grid.interfaces.ActorGrid.MasterGrid;
 
-public class Projectile extends AbstractActor {
+public class Projectile implements Actor {
 
 
 	private double mySpeed;
@@ -15,23 +16,14 @@ public class Projectile extends AbstractActor {
 	private double finalY;
 	//private Path myPath;
 	
-	public Projectile(MoveWithDestinationProperty m, 
-			HealthProperty h, List<IActProperty> properties) {	
+	public Projectile(MoveWithDestinationProperty m, HealthProperty h, List<IActProperty> properties) {	
 		//TODO: Complete
+		
 	}
 	
 	public Projectile(double endX, double endY) {
 		finalX = endX;
 		finalY = endY;
-	}
-	
-	/* (non-Javadoc)
-	 * @see gameengine.actors.management.Actor#act(java.lang.Object)
-	 */
-	@Override
-	public void act(MasterGrid grid) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	/* (non-Javadoc)
@@ -45,6 +37,15 @@ public class Projectile extends AbstractActor {
 
 	@Override
 	public void applyDamage(double health) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see gameengine.actors.management.Actor#act(java.lang.Object)
+	 */
+	@Override
+	public void act(Object grid) {
 		// TODO Auto-generated method stub
 		
 	}
