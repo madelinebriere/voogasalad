21dfa8f7f9d25cc6056e3550c1b55bffc582ae5f
Ab546 <ab546@duke.edu>
2017-04-04 23:45:13 -0400

    added functionality to levelvieweditor
    Former-commit-id: 5efbc0ff6e087d75238e4e71be33e581aef1e725

--- src/ui/authoring/EnemyEditorView.java
+++ src/ui/authoring/EnemyEditorView.java
@@ -2,7 +2,14 @@ package ui.authoring;
 
 import javafx.scene.layout.Pane;
 import ui.general.UIView;
+//sprite
+//health
+//speed
 
+//name
+//canattacktowers
+	//projectiles
+	//attack		
 public class EnemyEditorView extends Pane{
 
 }
