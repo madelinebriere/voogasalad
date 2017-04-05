5f61eb6f66212acab586c464553788a7233923ef
Hari <hari.rajan@duke.edu>
2017-04-04 23:16:36 -0400

    added id to constructors
    Former-commit-id: 25af60428a794943bc46292e104ae3468c5a1ae8

--- src/gameengine/actors/towers/Tower.java
+++ src/gameengine/actors/towers/Tower.java
@@ -8,8 +8,8 @@ import gameengine.grid.interfaces.ActorGrid.ReadableGrid;
 
 public abstract class Tower extends AbstractActor<ReadShootMoveGrid> {
 	
-	public Tower(HealthProperty health, IActProperty<? extends ReadableGrid>... properties) {
-		super(health);
+	public Tower(Integer id, HealthProperty health, IActProperty<? extends ReadableGrid>... properties) {
+		super(id, health);
 	}
 
 }
