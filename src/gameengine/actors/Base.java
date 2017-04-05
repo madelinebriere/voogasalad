ce0249fef69ad525bc303a4f0a8eda63feec8c0f
Gideon Pfeffer <gap16@duke.edu>
2017-04-04 21:45:10 -0400

    removed generics and had all actors extends MasterGrid
    Former-commit-id: 252d3b7345cc80e0e6440429a1b3ba8a7e2b07e7

--- src/gameengine/actors/Base.java
+++ src/gameengine/actors/Base.java
@@ -1,8 +1,5 @@
 package gameengine.actors;
 
-
-import gameengine.grid.interfaces.ActorGrid.ReadableGrid;
-
-public abstract class Base <T extends ReadableGrid> extends AbstractActor<T>{
+public abstract class Base extends AbstractActor{
 
 }
