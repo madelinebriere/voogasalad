781dd3f2ab98977e6b8bf2addf3aa90803bf0e99
Gideon Pfeffer <gap16@duke.edu>
2017-04-04 23:54:36 -0400

    Revert to 56e05fced
    Former-commit-id: 458e54907e5945f075b9e123446f6ac7c7219d97

--- src/gameengine/actors/Projectile.java
+++ src/gameengine/actors/Projectile.java
@@ -1,78 +1,51 @@
 package gameengine.actors;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import gameengine.actors.management.Actor;
 import gameengine.actors.properties.HealthProperty;
+import gameengine.actors.properties.IActProperty;
 import gameengine.actors.properties.MoveWithDestinationProperty;
-import gameengine.grid.interfaces.ActorGrid.ReadShootMoveGrid;
+import gameengine.grid.interfaces.ActorGrid.MasterGrid;
 
-public class Projectile extends AbstractActor<ReadShootMoveGrid> {
+public class Projectile extends Shot{
 
-	private boolean attacksEnemy;
-	private boolean attacksTower;
-	private boolean attacksBase;
-	private double myHitRadius;
-	private double myPower;
-	private MoveWithDestinationProperty myMovement;
 
-	public Projectile(Integer id, MoveWithDestinationProperty movement, HealthProperty health, 
-			boolean attacksEnemy, boolean attacksTower, boolean attacksBase, double power, double hitRadius) {
-		super(id, health);
-		myMovement = movement;
-		myHitRadius = hitRadius;
-		myPower = power;
-		this.attacksBase = attacksBase;
-		this.attacksEnemy = attacksEnemy;
-		this.attacksTower = attacksTower;
+	private double mySpeed;
+	private double finalX;
+	private double finalY;
+	//private Path myPath;
+	
+	public Projectile(MoveWithDestinationProperty m, 
+			HealthProperty h, List<IActProperty> properties){	
+		//TODO: Complete
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * 
+	public Projectile(double endX, double endY) {
+		finalX = endX;
+		finalY = endY;
+	}
+	
+	/* (non-Javadoc)
 	 * @see gameengine.actors.management.Actor#act(java.lang.Object)
 	 */
 	@Override
-	public void act(ReadShootMoveGrid grid) {
+	public void act(MasterGrid grid) {
 		// TODO Auto-generated method stub
-		if (attacksEnemy) {
-			Collection<Actor<?>> enemies = new ArrayList<>();// grid.getEnemiesInRadius() to apply damage
-			attack(enemies);
-		}
-		if (attacksTower) {
-			Collection<Actor<?>> towers = new ArrayList<>();// grid.getTowersInRadius() to apply damage
-			attack(towers);
-		}
-		if (attacksBase) {
-			Collection<Actor<?>> base = new ArrayList<>();// grid.getTowersInRadius() to apply damage
-			attack(base);
-		}
+		
 	}
 
-	private void attack(Collection<Actor<?>> actors) {
-		if (!isActive()) return;
-		for (Actor<?> a : actors) {
-			if (isActive()) {
-				a.applyDamage(myPower);
-				myHealth.apply(1);
-			} else {
-				break;
-			}
-		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see gameengine.actors.management.Actor#applyDamage(double)
+	/* (non-Javadoc)
+	 * @see gameengine.actors.management.Actor#isActive()
 	 */
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 	@Override
 	public void applyDamage(double health) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-
 }
--- src/gameengine/actors/properties/shoots/ShootableProperty.java
+++
@@ -1,9 +0,0 @@
-package gameengine.actors.properties.shoots;
-
-import gameengine.actors.properties.IActProperty;
-import gameengine.grid.interfaces.ActorGrid.ReadAndShootGrid;
-
-public interface ShootableProperty <G extends ReadAndShootGrid> extends IActProperty<G>{
-	
-	public boolean shoot(G grid);
-}
