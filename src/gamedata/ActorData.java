d4e8b50f0552f5e12ed831d167c06cb7a15e3c50
Madeline Briere <mfb33@duke.edu>
2017-04-06 15:42:08 -0400

    More changes to ActorData
    Former-commit-id: 93d9a1a7c621a72709f7d9505b4781a541936a55

--- src/gamedata/ActorData.java
+++ src/gamedata/ActorData.java
@@ -33,6 +33,22 @@ public class ActorData {
 	private BasicData basic;
 	
 	/**
+	 * These constructors/methods can be used in the Game Authoring Environment to build
+	 * the possible Troop, Tower, etc. options for the GamePlayer
+	 * 
+	 * This is the most basic constructor for the ActorData class. To add the "blueprints"
+	 * for any Actor to the backend, you must build an ActorData. 
+	 * 
+	 * The minimal ActorData requires:
+	 * 1) A basic actor type (e.g., Troop, Tower) which are used in the backend to
+	 * filter (e.g., to make sure that all Troops shoot only Towers.
+	 * 2) A BasicData object which represents the basic information about this
+	 * actor. This includes the name and imagepath of the game piece. 
+	 * 
+	 * This constructor defaults to a LimitedHealth HealthData (all Actors must
+	 * have some sort of Health, even if it is infinite). It also passes an empty array
+	 * as the properties of this Actor.
+	 * 
 	 * Easy implementation example:
 	 * 
 	 * Creation of a tower named Bob
@@ -42,11 +58,7 @@ public class ActorData {
 	 * @param data BasicData object with name and imagepath
 	 */
 	public ActorData(BasicActorType actor, BasicData data){
-		this(actor, data, new LimitedHealthData(HEALTH));
-	}
-	
-	public ActorData(BasicActorType actor, BasicData data, HealthData health){
-		this(actor, data, health, new Data[0]);
+		this(actor, data, new LimitedHealthData(HEALTH), new Data[0]);
 	}
 	
 	public ActorData(BasicActorType actor, BasicData data, HealthData health, Data... properties){
@@ -57,6 +69,22 @@ public class ActorData {
 		this.actor=actor;
 		this.basic = data;
 	}
+	
+	/**
+	 * For use in Game Authoring Environment.
+	 * 
+	 * Add a descriptor to this Actor blueprint
+	 * 
+	 * Examples:
+	 * PoisionData --> information about Poisoning
+	 * ShootData --> Information about how to shoot
+	 * 
+	 * @param property Data object to add to the ActorData's list
+	 */
+	public void addData(Data property){
+		myData.add(property);
+	}
+	
 	
 	/**
 	 * GamePlayer: Useful methods for accessing information about an ActorData
