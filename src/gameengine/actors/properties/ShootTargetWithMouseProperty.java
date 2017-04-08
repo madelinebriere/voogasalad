54bbc96572498cdfd6b24abeb81492ba60936a6c
Moses Wayne <msw38@duke.edu>
2017-04-08 15:41:08 -0400

    Adding changes to resolve some merge conflicts
    Former-commit-id: 397ab25c9a5a17038e915d5949f304ad7d9a4185

---
+++ src/gameengine/actors/properties/ShootTargetWithMouseProperty.java
@@ -0,0 +1,34 @@
+package gameengine.actors.properties;
+
+import java.util.Collection;
+
+import gamedata.composition.ShootData;
+import gameengine.actors.propertygen.ShootTargetProperty;
+import gameengine.grid.interfaces.ActorGrid.ReadAndSpawnGrid;
+import gameengine.grid.interfaces.Identifiers.Grid2D;
+
+public class ShootTargetWithMouseProperty<G extends ReadAndSpawnGrid> extends ShootTargetProperty<G>{
+
+	public ShootTargetWithMouseProperty(ShootData myData) {
+		super(myData);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Collection<Grid2D> getEnemyToShoot(Collection<Grid2D> points, Grid2D myPos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void spawnProjectiles(G grid, Collection<Grid2D> targets) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean isOn() {
+		return true;
+	}
+
+}
