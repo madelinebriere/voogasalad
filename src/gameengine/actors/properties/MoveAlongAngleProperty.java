12fda3946d7189d493d2febe4d6c2fc190bdc5fe
Moses Wayne <msw38@duke.edu>
2017-04-08 15:53:21 -0400

    Made changes to the actor classes for Gideon
    Former-commit-id: 46bc0bba4b4cd003f6785f8595b6358020f2e376

--- src/gameengine/actors/properties/MoveAlongAngleProperty.java
+++ src/gameengine/actors/properties/MoveAlongAngleProperty.java
@@ -1,5 +1,26 @@
 package gameengine.actors.properties;
 
-public class MoveAlongAngleProperty {
+import gameengine.actors.propertygen.IActProperty;
+import gameengine.grid.interfaces.ActorGrid.ReadAndMoveGrid;
+
+public class MoveAlongAngleProperty<G extends ReadAndMoveGrid> implements IActProperty<G>{
+
+	public double myPathAngle;
+	
+	public MoveAlongAngleProperty() {
+		
+	}
+	
+	@Override
+	public void action(G grid, Integer actorID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/gameengine/actors/properties/ShootTargetNearProperty.java
+++ src/gameengine/actors/properties/ShootTargetNearProperty.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Comparator;
 
 import gamedata.compositiongen.ShootData;
+import gameengine.actors.propertygen.IActProperty;
 import gameengine.actors.propertygen.ShootTargetProperty;
 import gameengine.grid.interfaces.ActorGrid.ReadAndSpawnGrid;
 import gameengine.grid.interfaces.Identifiers.Grid2D;
@@ -25,7 +26,10 @@ public class ShootTargetNearProperty<G extends ReadAndSpawnGrid> extends ShootTa
 
 	@Override
 	protected void spawnProjectiles(G grid, Collection<Grid2D> targets, double startX, double startY) {
-		grid.actorSpawnActor(getMyProjectile(), grid.getLocationOf(), startY)
+		targets.stream().forEach(target -> {
+			IActProperty<G> projectile = new
+			grid.actorSpawnActor(getMyProjectile(), startX, startY).accept(t);;
+		});
 		//targets.stream().forEach(point -> grid.spawn(newActor, startX, startY););
 	}
 
