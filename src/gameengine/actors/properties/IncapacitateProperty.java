7101e9e968cfa84332d9f008d70be85aae186ac2
Hari <hari.rajan@duke.edu>
2017-04-08 14:23:19 -0400

    pulled
    Former-commit-id: 7372772385d2ac6ef45796fa22121420fc5b182c

--- src/gameengine/actors/properties/IncapacitateProperty.java
+++ src/gameengine/actors/properties/IncapacitateProperty.java
@@ -2,7 +2,7 @@ package gameengine.actors.properties;
 
 import gameengine.grid.interfaces.ActorGrid.ReadableGrid;
 
-public class IncapacitateProperty implements IActProperty<ReadableGrid>{
+public class implements IActProperty<ReadableGrid>{
 
 	@Override
 	public void action(ReadableGrid grid, Integer actorID) {
