c40a510cc9e38b2374bb60a186b641cbdcfe235e
Madeline Briere <mfb33@duke.edu>
2017-04-08 00:26:15 -0400

    Small changes
    Former-commit-id: ed253fd7339e6bd4763c841142ee261449e53a39

--- src/gamedata/composition/ShootTargetNearData.java
+++ src/gamedata/composition/ShootTargetNearData.java
@@ -1,24 +1,8 @@
 package gamedata.composition;
 
 public class ShootTargetNearData extends ShootData{
-
-	private double myRange;
-	private int fireRate;
 	
 	public ShootTargetNearData(double myRange, int fireRate){
 		super(myRange, fireRate);
-	}
-	
-	public double getRange() {
-		return myRange;
-	}
-	public void setRange(double myRange) {
-		this.myRange = myRange;
-	}
-	public int getFireRate() {
-		return fireRate;
-	}
-	public void setFireRate(int fireRate) {
-		this.fireRate = fireRate;
 	}
 }
