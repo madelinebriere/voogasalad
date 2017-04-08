19d675e454e88d9e4db8faf97a4fa88b85dbcbde
Gideon Pfeffer <gap16@duke.edu>
2017-04-08 14:38:42 -0400

    added further controller interfaces
    Former-commit-id: a242c866318c3e22c4910a2d44a9759b900871c8

--- src/gameengine/grid/interfaces/controllergrid/ControllableGrid.java
+++ src/gameengine/grid/interfaces/controllergrid/ControllableGrid.java
@@ -1,11 +1,14 @@
 package gameengine.grid.interfaces.controllergrid;
 
 import gameengine.actors.management.Actor;
+import gameengine.grid.interfaces.Identifiers.Grid2D;
 
 public interface ControllableGrid extends SteppableGrid{
 	
 	void move(int ID, double newX, double newY);
 	void removeActor(int ID);
 	void controllerSpawnActor(Actor actor, int startX, int startY);
+	Grid2D getLocationOf(int ID);
+	boolean isValidLoc(double x, double y);
 	
 }
