9054ab08f271c034cd3661459baccc62057bb263
Madeline Briere <mfb33@duke.edu>
2017-04-11 19:05:24 -0400

    Small changes
    Former-commit-id: be18171c011c4ed168fc14812e8344b89e0561cc

--- src/gamedata/testers/PropertyFactoryTest.java
+++ src/gamedata/testers/PropertyFactoryTest.java
@@ -7,17 +7,25 @@ import org.junit.Test;
 import factories.PropertyFactory;
 import gamedata.composition.AfflictStatusData;
 import gamedata.composition.BaseDamageData;
+import gamedata.composition.DamageableData;
+import gamedata.composition.ImmuneHealthData;
+import gamedata.composition.LimitedHealthData;
+import gamedata.composition.MoveWithSetPathData;
+import gamedata.composition.ShootTargetFarData;
+import types.BasicActorType;
 
 public class PropertyFactoryTest {
 
 	@Test
 	public void test() {
 		PropertyFactory p = new PropertyFactory();
-		p.make("AfflictStatusProperty", new AfflictStatusData());
-		p.make("BaseDamageProperty", new BaseDamageData());
-		p.make("AfflictStatusProperty", new AfflictStatusData());
-		p.make("AfflictStatusProperty", new AfflictStatusData());
-		
+		assertNotEquals(p.make("AfflictStatusProperty", new AfflictStatusData()),null);
+		assertNotEquals(p.make("BaseDamageProperty", new BaseDamageData(10, BasicActorType.Tower)), null);
+		assertNotEquals(p.make("DamageableProperty", new DamageableData(10, 10, BasicActorType.Tower)),null);
+		assertNotEquals(p.make("ImmuneHealthProperty", new ImmuneHealthData()), null);
+		assertNotEquals(p.make("LimitedHealthProperty", new LimitedHealthData(10)), null);
+		assertNotEquals(p.make("MoveWithSetPathProperty", new MoveWithSetPathData(null, 10)), null);
+		assertNotEquals(p.make("ShootTargetFarProperty", new ShootTargetFarData(10, 10, BasicActorType.Tower, 10)), null);
 	}
 
 }
