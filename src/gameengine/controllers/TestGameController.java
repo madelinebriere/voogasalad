a147ec75e3ae4dc8a26d29ad793dae3d60ee332f
SARAH ZHOU <sz107@duke.edu>
2017-04-11 00:39:45 -0400

    updated GameCtronller
    Former-commit-id: b16db9210c7a783ff06b2052725d888fd7835ab4

--- src/gameengine/controllers/TestGameController.java
+++ src/gameengine/controllers/TestGameController.java
@@ -1,6 +1,12 @@
 package gameengine.controllers;
 
+import javafx.stage.Stage;
+
 public class TestGameController {
 
-	public TestGameController
+	public static void main(String[] args) {
+		GameController controller = new GameController();
+		controller.start(new Stage());
+		controller.step();
+	}
 }
