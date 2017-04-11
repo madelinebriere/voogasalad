647158379980fe17f00601e55c07ed05c30c9c97
Madeline Briere <mfb33@duke.edu>
2017-04-10 20:54:31 -0400

    Changes to data package
    Former-commit-id: 7f5c90de05d6b152b7c6713b3e1e6ac84c9bdb33

---
+++ src/gamedata/composition/BaseDamageData.java
@@ -0,0 +1,34 @@
+package gamedata.composition;
+
+import gamedata.compositiongen.Data;
+import types.BasicActorType;
+
+public class BaseDamageData implements Data {
+
+	private double myRadius;
+	private BasicActorType myTarget;
+
+	public BaseDamageData(double radius, BasicActorType type) {
+		myRadius = radius;
+		myTarget = type;
+	}
+
+	public double getMyRadius() {
+		return myRadius;
+	}
+
+	public void setMyRadius(double myRadius) {
+		this.myRadius = myRadius;
+	}
+
+	public BasicActorType getMyTarget() {
+		return myTarget;
+	}
+
+	public void setMyTarget(BasicActorType myTarget) {
+		this.myTarget = myTarget;
+	}
+	
+	
+	
+}
