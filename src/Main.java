d0cd5aa944ee02cc25d23ea853b258385666aefe
SARAH ZHOU <sz107@duke.edu>
2017-04-11 00:19:28 -0400

    edited GameController to work with UI updates
    Former-commit-id: e6668125caed05c0bed102d1ab6ffaca7e28583b

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,7 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		GameController controller = new GameController();
-		controller.start();
+		
 	}
 
 }
