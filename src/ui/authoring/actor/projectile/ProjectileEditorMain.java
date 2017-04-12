396c3b1dc257a81764a395613e01edff9547c87c
Gideon Pfeffer <gap16@duke.edu>
2017-04-11 22:38:34 -0400

    added back button
    Former-commit-id: 1a2966e0ca17dec89da98e6ebbb52de674fed529

--- src/ui/authoring/actor/projectile/ProjectileEditorMain.java
+++ src/ui/authoring/actor/projectile/ProjectileEditorMain.java
@@ -1,18 +1,28 @@
 package ui.authoring.actor.projectile;
 
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import ui.authoring.delegates.PopViewDelegate;
+import ui.general.ImageButton;
+import util.Location;
 
 public class ProjectileEditorMain extends AnchorPane{
 	private PopViewDelegate delegate;
 	
 	public ProjectileEditorMain(PopViewDelegate delegate){
+		super();
+		
 		this.delegate = delegate;
 		this.widthProperty().divide(5);
+		setupBackButton();
 	}
 	
-	public void close(){
-		delegate.closeView(this);
+	private void setupBackButton() {
+		ImageButton b = new ImageButton("back_icon.png", new Location(30., 30.));
+		AnchorPane.setTopAnchor(b, 4.0);
+		AnchorPane.setLeftAnchor(b, 4.0);
+		b.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> delegate.closeView(this));
+		this.getChildren().add(b);
 	}
 	
 	
