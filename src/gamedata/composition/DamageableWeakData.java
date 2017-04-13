5de786f3390dfc0fcc75248e8968728e885dc6f9
Madeline Briere <mfb33@duke.edu>
2017-04-13 13:51:34 -0400

    bug fix
    Former-commit-id: b9495c58da4122b96e3a32628187fd40e1840fe7

---
+++ src/gamedata/composition/DamageableWeakData.java
@@ -0,0 +1,16 @@
+package gamedata.composition;
+
+import gamedata.compositiongen.*;
+import types.BasicActorType;
+
+public class DamageableWeakData extends DamageableData{
+	public DamageableWeakData(){
+		super();
+	}
+
+	public DamageableWeakData(Double myPower, Double myHitRadius, BasicActorType... myEnemyTypes) {
+		super(myPower, myHitRadius, myEnemyTypes);
+	}
+	
+}
+
