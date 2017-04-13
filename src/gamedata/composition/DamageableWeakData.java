b17b6757b90be5ca62ecd2691a84751a1395f1f2
Madeline Briere <mfb33@duke.edu>
2017-04-13 13:54:53 -0400

    Small fix
    Former-commit-id: c3bd92732ea4721f5859c1e6f67af7cb8f929721

---
+++ src/gamedata/composition/DamageableWeakData.java
@@ -0,0 +1,18 @@
+package gamedata.composition;
+
+import gamedata.compositiongen.*;
+import types.BasicActorType;
+
+public class DamageableWeakData extends DamageableData {
+
+	public DamageableWeakData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DamageableWeakData(Double myPower, Double myHitRadius, BasicActorType... myEnemyTypes) {
+		super(myPower, myHitRadius, myEnemyTypes);
+		// TODO Auto-generated constructor stub
+	}
+
+}
