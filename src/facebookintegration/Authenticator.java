7521182b64174f3628df2ca5de6756bac1934d5c
Gideon Pfeffer <gap16@duke.edu>
2017-04-15 20:11:28 -0400

    added profile pic extraction
    Former-commit-id: bcc1b5917b66dc053fbb7ca75cc1084d54caedf9

---
+++ src/facebookintegration/Authenticator.java
@@ -0,0 +1,57 @@
+package facebookintegration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.Version;
+import com.restfb.json.JsonObject;
+import com.restfb.types.User;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Authenticator {
+	private String domain, appID, authURL;
+	private WebDriver driver;
+	
+	
+	public Authenticator(){
+		domain = "http://www.cs.duke.edu/courses/compsci308/spring17/classwork/";
+		appID = "164672860722038";
+		authURL = "https://graph.facebook.com/oauth/authorize?type=user_agent&client_id="+appID+
+				"&redirect_uri="+domain+"&scope=email,user_about_me,user_friends,user_birthday,user_photos,public_profile,";
+		
+		System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
+		driver = new ChromeDriver();
+	}
+	
+	public ImageView access(){
+		String accessToken;
+		driver.get(authURL);
+		
+		while(true){
+			if(!driver.getCurrentUrl().contains("facebook.com")){
+				String url = driver.getCurrentUrl();
+				accessToken = url.replaceAll(".*#access_token=(.+)&.*", "$1");
+				driver.quit();
+				break;
+			}
+		}
+		
+		FacebookClient client = new DefaultFacebookClient(accessToken, Version.LATEST);
+	
+		JsonObject picture = 
+		      client.fetchObject("me/picture", 
+			      JsonObject.class, Parameter.with("redirect","false"));
+		
+		String url = picture.get("data").asObject().get("url").asString();
+		System.out.println(url);
+		ImageView imageView = new ImageView(new Image(url));
+		
+		return imageView;
+	}
+
+}
