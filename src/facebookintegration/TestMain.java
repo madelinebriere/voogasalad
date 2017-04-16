ea9205b664c0d7ef0b104b19ce14c24813cbe766
Gideon Pfeffer <gap16@duke.edu>
2017-04-15 20:11:28 -0400

    added profile pic extraction
    Former-commit-id: 8d236b78f1f49ac6c86de7edc1986aa49c2b7b6d

---
+++ src/facebookintegration/TestMain.java
@@ -0,0 +1,38 @@
+package facebookintegration;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class TestMain extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        StackPane root = new StackPane();
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+                Authenticator a = new Authenticator();
+                root.getChildren().add(a.access());
+            }
+        });
+       
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+	}
+
+}
