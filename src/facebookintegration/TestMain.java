8eed271b3d22c162ebcb8a9579ac5848274bfe80
Gideon Pfeffer <gap16@duke.edu>
2017-04-17 12:44:24 -0400

    got posting on facebook to work
    Former-commit-id: b36317cb60f9b498ed0e1183648109b8e79af364

--- src/facebookintegration/TestMain.java
+++ src/facebookintegration/TestMain.java
@@ -3,8 +3,10 @@ package facebookintegration;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -16,18 +18,23 @@ public class TestMain extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Get FB Set Up!");
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Get FB Set Up!");
         StackPane root = new StackPane();
+        Authenticator a = null;
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+        	
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                Authenticator a = new Authenticator();
-                root.getChildren().add(a.getProfilePicFromClient(a.access()));
-                root.getChildren().remove(btn);
+	            Authenticator a = new Authenticator();
+	            a.setUp();
+                ImageView pic = a.getProfilePicFromClient();
+	            pic.setFitHeight(50);
+	            pic.setFitWidth(50);
+	            StackPane.setAlignment(pic, Pos.TOP_CENTER);
+	            root.getChildren().add(a.getProfilePicFromClient());
+	            btn.setText("Post What's Up to FB");
             }
         });
        
