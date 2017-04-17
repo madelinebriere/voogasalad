f22e8d7ffb0b412b86d3cffda6c63d459b43172a
Madeline Briere <mfb33@duke.edu>
2017-04-17 14:25:27 -0400

    LevelEditor
    Former-commit-id: a582038c5bc1736cbac40a2199895483d1c60805

--- src/ui/authoring/level/LevelEditorMenu.java
+++ src/ui/authoring/level/LevelEditorMenu.java
@@ -1,52 +1,31 @@
 package ui.authoring.level;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-import builders.OptionGenerator;
-
-import java.util.Map.Entry;
+import javax.swing.JTabbedPane;
 
 import gamedata.ActorData;
-import gamedata.FieldData;
-import gamedata.PathData;
-import gameengine.grid.interfaces.Identifiers.Grid2D;
+import gamedata.LevelData;
+import gamedata.WaveData;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Dialog;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.util.Pair;
-import types.BasicActorType;
 import ui.Preferences;
-import ui.authoring.actor.ActorEditorView;
 import ui.authoring.delegates.PopViewDelegate;
 import ui.general.CustomColors;
 import ui.general.ImageButton;
@@ -54,62 +33,133 @@ import ui.general.UIHelper;
 import util.Location;
 
 public class LevelEditorMenu extends AnchorPane {
+
 	PopViewDelegate myDelegate;
+	private WaveData editWave;
+	private LevelData myData;
 	private List<ActorData> activeEnemies = new ArrayList<ActorData>();
-	private List<Integer> activePaths = new ArrayList<Integer>();
 	private Collection<ActorData> enemies;
-	ScrollPane bottomSide;
-	StackPane topSide;
+	private Collection<StackPane> waveBoxes;
+	ScrollPane waves;
+	ScrollPane actors;
 	
-	private PathData myPathData;
-	public LevelEditorMenu(PopViewDelegate d, Collection<ActorData> enemies,PathData pathData) {
+	//TODO: PathData needed?
+	public LevelEditorMenu(PopViewDelegate delegate, Collection<ActorData> enemies, LevelData level) {
 		super();
-		myDelegate = d;
-		this.enemies=enemies;
-		myPathData=pathData;
+		myDelegate = delegate;
+		waveBoxes = new ArrayList<StackPane>();
+		this.enemies = enemies;
+		editWave = null;
+		myData = level;
 		setupViews();
-		
-		
+		populateViews();
 	}
 
 	private void setupViews() {
-	 bottomSide = new ScrollPane();
-		 topSide = new StackPane();
-		setupSides(bottomSide, topSide);
-
+		waves = new ScrollPane();
+		actors = new ScrollPane();
+		setupBack(actors, waves);
+	}
+	
+	private void populateViews(){
 		setupBackButton();
-		Map<String, List<FieldData>> map = OptionGenerator.getPropertyTypesWithArgs();
 		populateEnemies();
-		populatePaths();
+		populateWaves();
 	}
-	private  void populatePaths(){
-		VBox root=new VBox();
-		Text label=new Text("Select Active Path");
-		root.setAlignment(Pos.CENTER);
-		ComboBox<Integer>pathChoice=new ComboBox<Integer>();
-		pathChoice.getItems().addAll(myPathData.getMyPaths().keySet());
-		pathChoice.valueProperty().addListener((x,y,newValue)->{choosePath(newValue);
-		System.out.println("sdsa"+myPathData.getMyPaths());	
-		});
-		root.getChildren().add(label);
-		root.getChildren().add(pathChoice);
-	topSide.getChildren().add(root);
+	
+	private void populateWaves(){
+		HBox root=new HBox();
+		root.setSpacing(30);
+		int numWaves = myData.getNumWaves();
+		for(int i=0; i<numWaves; i++){
+			StackPane wave = nextWave();
+			waveBoxes.add(wave);
+			root.getChildren().add(wave);
+		}
+		addWaveButton(root);
 	}
-	private void choosePath(Integer newValue){
-		activePaths=new ArrayList<Integer>();
-		activePaths.add(newValue);
+	
+	private void addWaveButton(HBox root){
+		StackPane newWave = UIHelper.buttonStack(e->addNewWave(), 
+				Optional.of(label("Add Wave")), 
+				Optional.of(image("add_icon.png")),
+				Pos.CENTER_RIGHT, true);
+		newWave.setPrefHeight(56);
+		VBox.setMargin(newWave, new Insets(8));
+		root.getChildren().add(newWave);
+		waves.setContent(root);
 	}
+	
 	private  void populateEnemies(){
 		HBox root=new HBox();
-		root.setSpacing(10);
+		root.setSpacing(30);
 		for(ActorData enemy:enemies){
 			ImageView image=new ImageView(new Image(enemy.getImagePath()));
-			root.getChildren().add(UIHelper.buttonStack(e->toggleActive(enemy), Optional.of(new Label(enemy.getName())), Optional.of(image), Pos.CENTER, true));
-			
+			root.getChildren().add(UIHelper.buttonStack(e->toggleActive(enemy),
+					Optional.of(label(enemy.getName())), Optional.of(image), Pos.CENTER, true));
 		}
-		bottomSide.setContent(root);
+		actors.setContent(root);
+	}
+	
+	private Label label(String title){
+		Label lbl = new Label(title);
+		lbl.setTextFill(CustomColors.GREEN_100);
+		lbl.setFont(Preferences.FONT_SMALL_BOLD);
+		return lbl;
+	}
+	
+	private ImageView image(String imagePath){
+		Image img = new Image(imagePath);
+		ImageView imageView = new ImageView(img);
+		imageView.setFitWidth(40);
+		imageView.setPreserveRatio(true);
+		return imageView;
+	}
+	
+	private HBox generateHBox(){
+		HBox root=new HBox();
+		root.setSpacing(30);
+		return root;
+	}
+	
+	private void addNewWave(){
+		HBox root= generateHBox();
+		StackPane newWave = nextWave();
+		Node content = waves.getContent();
+		root.getChildren().add(content);
+		root.getChildren().add(newWave);
+		waveBoxes.add(newWave);
+		myData.addWave(new WaveData());
+		waves.setContent(root);
+	}
+	
+	private void selectWave(StackPane selected, int wave){
+
+		editWave = myData.getMyWaves().get(wave);
+		
+		HBox root=generateHBox();
+		addWaveButton(root);
+		
+		for(StackPane box: waveBoxes){
+			box.setOpacity(1);
+		}
+		selected.setOpacity(.5);
+		root.getChildren().addAll(waveBoxes);
 		
 	}
+	
+	private StackPane nextWave(){
+		StackPane nextWave= UIHelper.buttonStack(e->{},  
+				Optional.of(label(String.format("      Wave %d       ", myData.getNumWaves()+1))), 
+				Optional.ofNullable(null),Pos.CENTER_RIGHT, true);
+		nextWave.addEventHandler(MouseEvent.MOUSE_CLICKED, 
+				e -> selectWave(nextWave, myData.getNumWaves()-1));
+		nextWave.setPrefHeight(56);
+		VBox.setMargin(nextWave, new Insets(8));
+		return nextWave;
+	}
+	
+	
 	private void toggleActive(ActorData enemy){
 		if(activeEnemies.contains(enemy)){
 			activeEnemies.remove(enemy);
@@ -117,7 +167,6 @@ public class LevelEditorMenu extends AnchorPane {
 		else{
 			activeEnemies.add(enemy);
 		}
-		System.out.println(activeEnemies);
 	}
 
 	private void setupBackButton() {
@@ -127,33 +176,34 @@ public class LevelEditorMenu extends AnchorPane {
 		b.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> myDelegate.closeView(this));
 		this.getChildren().add(b);
 	}
-
-	private void setupSides(ScrollPane bottomSide, StackPane topSide) {
-
+	
+	private void setupBack(ScrollPane bottomSide, ScrollPane topSide){
 		double inset = 12.0;
-
-		AnchorPane.setLeftAnchor(topSide, inset);
-		AnchorPane.setLeftAnchor(bottomSide, inset);
-		AnchorPane.setRightAnchor(topSide, 48.0);
-		AnchorPane.setRightAnchor(bottomSide, 48.0);
+		setVerticalAnchors(inset, bottomSide, topSide);
+		setupBar(inset,bottomSide);
+		setupBar(inset, topSide);
+		this.getChildren().addAll(bottomSide, topSide);
+	}
+	
+	private void setVerticalAnchors(double inset, ScrollPane bottomSide, ScrollPane topSide){
 		AnchorPane.setTopAnchor(topSide, inset);
 		AnchorPane.setBottomAnchor(bottomSide, inset);
-		topSide.setBackground(new Background(new BackgroundFill(CustomColors.GREEN_200,null,null)));
-		topSide.setStyle("-fx-background-color: #" + UIHelper.colorToHex(CustomColors.GREEN_200) + ";");
-		topSide.setStyle("-fx-background: #" + UIHelper.colorToHex(CustomColors.GREEN_200) + ";");
-		bottomSide.setStyle("-fx-background-color: #" + UIHelper.colorToHex(CustomColors.GREEN_200) + ";");
-		bottomSide.setStyle("-fx-background: #" + UIHelper.colorToHex(CustomColors.GREEN_200) + ";");
+	}
+	
+	private void setupBar(double inset, ScrollPane pane){
+		AnchorPane.setLeftAnchor(pane, inset);
+		
+		AnchorPane.setRightAnchor(pane, 48.0);
+		
+		pane.setBackground(new Background(new BackgroundFill(CustomColors.GREEN_200,null,null)));
+		pane.setStyle("-fx-background: #" + UIHelper.colorToHex(CustomColors.GREEN_200) + ";");
 
-		bottomSide.setHbarPolicy(ScrollBarPolicy.NEVER);
-		//topSide.setHbarPolicy(ScrollBarPolicy.NEVER);
-
-		UIHelper.setDropShadow(topSide);
-		UIHelper.setDropShadow(bottomSide);
-		bottomSide.prefHeightProperty().bind(this.heightProperty().divide(3.0 / 2).subtract(inset * 3 / 2));
-		topSide.prefHeightProperty().bind(this.heightProperty().divide(3.0).subtract(inset * 3 / 2));
-
-		this.getChildren().addAll(bottomSide, topSide);
-
+		UIHelper.setDropShadow(pane);
+		pane.prefHeightProperty().bind(this.heightProperty().divide(2.0).subtract(inset * 3 / 2));
+	}
+	
+	public LevelData getLevelData(){
+		return myData;
 	}
 
 }
