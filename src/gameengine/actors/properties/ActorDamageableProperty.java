12e850e36d63c3405ece445e68a489a1facf5287
Madeline Briere <mfb33@duke.edu>
2017-04-17 22:53:44 -0400

    Fixes to GameData for LineageData
    Former-commit-id: 4f6df4436215979bec257ce882c75f52404accb8

--- src/gameengine/actors/properties/ActorDamageableProperty.java
+++ src/gameengine/actors/properties/ActorDamageableProperty.java
@@ -2,38 +2,34 @@ package gameengine.actors.properties;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
+import gamedata.composition.ActorDamageableData;
 import gameengine.actors.propertygen.IActProperty;
 import gameengine.grid.interfaces.ActorGrid.ReadAndDamageGrid;
 import types.BasicActorType;
 
 public class ActorDamageableProperty<G extends ReadAndDamageGrid> implements IActProperty<G> {
-
+	
 	private Collection<BasicActorType> myTypes;
 	private double hitBox;
-
-	public ActorDamageableProperty(Double radius, BasicActorType... types) {
-		hitBox = radius;
+	
+	public ActorDamageableProperty(ActorDamageableData data) {
+		hitBox = data.getMyHitRadius();
 		myTypes = new ArrayList<>();
-		for (BasicActorType t : types) {
+		for (BasicActorType t : data.getMyEnemyTypes()) {
 			myTypes.add(t);
 		}
 	}
-
 	public void action(G grid, Integer actorID) {
-		myTypes.stream()
-				.forEach(type -> grid
-						.getActorDamagablesInRadius(grid.getLocationOf(actorID).getX(),
-								grid.getLocationOf(actorID).getX(), hitBox, type)
-						.forEach((health, damage) -> {
-							grid.getMyDamageable(actorID).accept(health);
-							damage.accept(health);
-						}));
+		System.out.println("Wjat is wrong?");
+		/*
+		myTypes.stream().forEach(type -> grid.getActorDamagablesInRadius(grid.getLocationOf(actorID).getX(),
+				grid.getLocationOf(actorID).getX(), hitBox, type).forEach((health, damage) -> {
+					grid.getMyDamageable(actorID).accept(health);
+					damage.accept(health);
+				}));*/
 	}
-
 	@Override
 	public boolean isOn() {
 		return true;
 	}
-
 }
