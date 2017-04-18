e819cb3947cf6d281dcba46138639251ca380b0a
Madeline Briere <mfb33@duke.edu>
2017-04-17 22:11:21 -0400

    Actor Damageable
    Former-commit-id: 3713f73c4c35c67c67198b8c0295a0af7bb93985

---
+++ src/gamedata/compositiongen/ActorDamageableData.java
@@ -0,0 +1,44 @@
+package gamedata.compositiongen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gamedata.compositiongen.Data;
+import types.BasicActorType;
+
+public class ActorDamageableData implements Data {
+	
+	private double myHitRadius;
+	private List<BasicActorType> myEnemyTypes;
+	
+	public ActorDamageableData(){
+		this(0.0, new BasicActorType[0]);
+	}
+	
+	public ActorDamageableData(Double myHitRadius, BasicActorType... myEnemyTypes) {
+		this.myHitRadius = myHitRadius;
+		this.myEnemyTypes = new ArrayList<>();
+		for (BasicActorType e : myEnemyTypes) {
+			this.myEnemyTypes.add(e);
+		}
+	}
+	
+	public String getCategory(){
+		return "Abilities to be Damaged";
+	}
+	
+	public double getMyHitRadius() {
+		return myHitRadius;
+	}
+	public void setMyHitRadius(double myHitRadius) {
+		this.myHitRadius = myHitRadius;
+	}
+	public List<BasicActorType> getMyEnemyTypes() {
+		return myEnemyTypes;
+	}
+	public void setMyEnemyTypes(List<BasicActorType> myEnemyTypes) {
+		this.myEnemyTypes = myEnemyTypes;
+	}
+	
+	
+}
