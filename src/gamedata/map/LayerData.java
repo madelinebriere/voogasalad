a4fee36a64bb33df4c875f61b80d9faf369dbed4
ANH TRINH <ANT19@duke.edu>
2017-04-19 22:42:54 -0400

    added in layer shit
    Former-commit-id: f1368a9f8400746a798dea5133062920f0bc6475

--- src/gamedata/map/LayerData.java
+++ src/gamedata/map/LayerData.java
@@ -1,13 +1,27 @@
 package gamedata.map;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author TNK
+ * @author Anh
+ *
+ */
 public class LayerData {
 	
 	private List<PolygonData> myPolygons;
 	
 	public LayerData(List<PolygonData> polygons){
 		myPolygons = polygons;
+	}
+	
+	/**
+	 * default for LayerData: only has 1 polygon which is the whole screen
+	 */
+	public LayerData(){
+		myPolygons = new ArrayList<PolygonData>();
+		myPolygons.add(new PolygonData());
 	}
 	
 	public List<PolygonData> getMyPolygons(){
