507942c6fdc92fba82bf57010b4ffab28e720159
Moses Wayne <msw38@duke.edu>
2017-04-20 16:07:29 -0400

    Big refactoring commit, should be non functional until data objects are made for the new shoot properties
    Former-commit-id: db2b9c5325a1d145f958027bbe7f681319820ad5

--- src/gameengine/actors/properties/ShootTargetFarProperty.java
+++ src/gameengine/actors/properties/ShootTargetFarProperty.java
@@ -5,12 +5,11 @@ import java.util.Collection;
 import java.util.Comparator;
 
 import gamedata.composition.ShootTargetFarData;
-import gameengine.actors.propertygen.ShootTargetProperty;
 import gameengine.grid.interfaces.ActorGrid.ReadAndSpawnGrid;
 import gameengine.grid.interfaces.Identifiers.Grid2D;
 import util.PathUtil;
 
-public class ShootTargetFarProperty<G extends ReadAndSpawnGrid> extends ShootTargetProperty<G> {
+public class ShootTargetFarProperty<G extends ReadAndSpawnGrid> extends ShootTargetLineProperty<G> {
 
 	public ShootTargetFarProperty(ShootTargetFarData myData) {
 		super(myData);
