0d6f4f0320b3c75a7b18639d0e1768590f9ccf70
Madeline Briere <mfb33@duke.edu>
2017-04-20 16:37:02 -0400

    Heat seeking
    Former-commit-id: ecb07480628de19d8650352dec1a9562d9208725

---
+++ src/gamedata/compositiongen/ShootTargetLineData.java
@@ -0,0 +1,17 @@
+package gamedata.compositiongen;
+
+import types.BasicActorType;
+
+public abstract class ShootTargetLineData extends ShootData {
+
+	public ShootTargetLineData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ShootTargetLineData(Double range, Integer rate, BasicActorType type, Integer projectile, Double speed) {
+		super(range, rate, type, projectile, speed);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
