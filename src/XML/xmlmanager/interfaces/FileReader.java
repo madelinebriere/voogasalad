7ab1e1e438c0ea9e48cc325bfaecc066966cf4f5
Gideon Pfeffer <gap16@duke.edu>
2017-04-21 12:42:45 -0400

    added exception handling to the package manager
    Former-commit-id: 57df5747c6b95db1fdfc604236c8a2cf4e85a4bf

--- src/XML/xmlmanager/interfaces/FileReader.java
+++ src/XML/xmlmanager/interfaces/FileReader.java
@@ -1,9 +1,11 @@
 package XML.xmlmanager.interfaces;
 
+import java.io.IOException;
+
 public interface FileReader {
 
 	boolean directoryExists(String filepathToDir);
 	
-	String getFileContent(String filepathToDir, String filename);
+	String getFileContent(String filepathToDir, String filename) throws IOException;
 	
 }
