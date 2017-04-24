f9e68f75d15c50b8156bbaec1a90a69dcbe81fef
Vishnu Gottiparthy <vtg4@duke.edu>
2017-04-23 23:29:04 -0400

    Login handler changes
    Former-commit-id: 4a1a2897146d89294c9cf222f2da3ba7068cfdd6

--- src/ui/handlers/LoginHandler.java
+++ src/ui/handlers/LoginHandler.java
@@ -5,6 +5,12 @@ import ui.player.users.User;
 public interface LoginHandler {
 	
 	public User getActiveUser();
-	
 	public void showProfile();
+	public void returnToMain();
+	public void gotoSignupPage();
+	public Boolean login(String username, String password);
+	public User findUser(String username);
+	public void setActiveUser(User user);
+	public void gotoAuth();
+	public void gotoGameSelector();
 }
