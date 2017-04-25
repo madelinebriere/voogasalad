89e98a17a7585849a46a021cf747469606a9c886
Anngelyque <ams191@duke.edu>
2017-04-25 17:55:37 -0400

    logout
    Former-commit-id: bf5900728d9821ebadb5069b6e3b38ab7de5ace2

--- src/ui/player/inGame/GameScreen.java
+++ src/ui/player/inGame/GameScreen.java
@@ -85,7 +85,7 @@ public class GameScreen extends GenericGameScreen
 	private void setup() {
 		setupPanels();
 		setupHUD();
-		getSettingsPane().setReturnToMain(returnToMain());
+		setReturnToMain(e -> loginhandler.returnToMain());
 	}
 	
 	private void setupPanels() {
