688f9c313bd003e3f836de1d9e5a782fab08ce4f
Talha Koc <talha.koc@duke.edu>
2017-04-28 23:05:05 -0400

    map
    Former-commit-id: 88adcc7f234f6ea640a4645efdde9abf43c8fac0

--- src/ui/authoring/map/layer/path/Path.java
+++ src/ui/authoring/map/layer/path/Path.java
@@ -7,6 +7,7 @@ import gameengine.grid.interfaces.Identifiers.Grid2D;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
+import ui.general.CustomColors;
 
 public class Path {
 	
@@ -14,11 +15,24 @@ public class Path {
 	private List<Line> myLines = new ArrayList<>();
 	
 	public void addPointTo(Point point, Pane pane){
-		
-		Point previous = myPoints.get(myPoints.size() - 1);
-		Line line = new Line(previous.getCenterX(), previous.getCenterY(),
+		pane.getChildren().add(point);
+		myPoints.add(point);
+		Line line = new Line(point.getCenterX(), point.getCenterY(),
 				 point.getCenterX(), point.getCenterY());
+		line.setStrokeWidth(3);
+		line.setFill(CustomColors.BLACK_GRAY);
+		pane.setOnMouseMoved(event -> {
+			line.setEndX(event.getX());
+			line.setEndY(event.getY());
+		});
+		pane.setOnMouseDragged(event -> {
+			line.setEndX(event.getX());
+			line.setEndY(event.getY());
+		});
 		pane.getChildren().add(line);
+		myLines.add(line);
+		System.out.println(myLines.size() + " size of lines");
+		System.out.println(myPoints.size() + " size of points");
 	}
 	
 	public List<Point> getPoints(){
