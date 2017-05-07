cc3c5a5724e3667a9eb2da5d4634e4c1a0eac147
Gideon Pfeffer <gap16@duke.edu>
2017-05-07 13:03:30 -0400

    commented out the new exception
    Former-commit-id: bfbafcd9c67dd4e3a90d43a21b11d091aec83bbc

--- src/gameengine/grid/exceptions/InvalidIDException.java
+++ src/gameengine/grid/exceptions/InvalidIDException.java
@@ -1,5 +1,11 @@
 package gameengine.grid.exceptions;
-
+/**
+ * 
+ * @author Gideon
+ *
+ * This exception is thrown when more than one actor shares the same ID
+ * as well as when someone tries to access an actor that is currently not on the grid
+ */
 public class InvalidIDException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
