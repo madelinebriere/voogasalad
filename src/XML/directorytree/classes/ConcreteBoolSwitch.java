057e24ea14565ed27617ff33177c6146036129b9
Gideon Pfeffer <gap16@duke.edu>
2017-05-06 22:32:31 -0400

    added comments to all classes that were missing comments
    Former-commit-id: 4fc7eb4ed4a2404ebe1ce8ee86cfc2f1c93f1250

--- src/XML/directorytree/classes/ConcreteBoolSwitch.java
+++ src/XML/directorytree/classes/ConcreteBoolSwitch.java
@@ -2,6 +2,15 @@ package XML.directorytree.classes;
 
 import XML.directorytree.interfaces.BoolSwitch;
 
+/**
+ * 
+ * @author Gideon
+ *
+ * This class is a concrete implementation of {@link BoolSwitch}
+ * 
+ * It is intended to keep track of a boolean and toggle it
+ * 
+ */
 public class ConcreteBoolSwitch implements BoolSwitch{
 
 	private boolean state;
@@ -10,24 +19,34 @@ public class ConcreteBoolSwitch implements BoolSwitch{
 		this.state = startState;
 	}
 
+	/**
+	 * Sets the boolean to true
+	 * 
+	 * See {@link BoolSwitch}
+	 */
 	@Override
 	public void setTrue() {
 		state = true;
 	}
 
+	/**
+	 * Sets the boolean to false
+	 * 
+	 * See {@link BoolSwitch}
+	 */
 	@Override
 	public void setFalse() {
 		state = false;
 	}
 
+	/**
+	 * Returns the current state of the switch 
+	 * 
+	 * See {@link BoolSwitch}
+	 */
 	@Override
 	public boolean getState() {
 		return state;
 	}
-	
-	
-	
-	
-	
 }
 
--- src/XML/xmlmanager/classes/NewDirectoryHelper.java
+++ src/XML/xmlmanager/classes/NewDirectoryHelper.java
@@ -10,17 +10,41 @@ import java.util.List;
 import XML.xmlmanager.exceptions.IllegalFileException;
 import XML.xmlmanager.exceptions.InvalidRootDirectoryException;
 
-public class NewDirectoryCreator extends AbstractFileHelper{
+/**
+ * @author Gideon
+ *
+ * This class is one flavor of the DirectoryFileMansger
+ * This class is to be used when you want to give exclusive access
+ * to a directory that doesn't already exist.
+ * This class makes the directory upon .
+ * {@link ExistingDirectoryHelper} should
+ * be used when you want to add to an existing directory
+ * 
+ * This class provides protection from writing, reading, adding,
+ * and deleting from just any file
+ */
+
+public class NewDirectoryHelper extends AbstractFileHelper{
 	
 	private List<String> addedFiles;
 	private String rootDir;
 	
-	public NewDirectoryCreator(String baseDirectoryPath, String newRootDirectoryName) throws InvalidRootDirectoryException, IOException{
+	/**
+	 * @param baseDirectoryPath the directory to add the new directory in
+	 * @param newRootDirectoryName the new directories name
+	 * @throws InvalidRootDirectoryException when the baseDirectory doesn't exist
+	 * @throws IOException when the new directory could not be added
+	 */
+	public NewDirectoryHelper(String baseDirectoryPath, String newRootDirectoryName) throws InvalidRootDirectoryException, IOException{
 		rootDir = baseDirectoryPath + "/" + newRootDirectoryName;
 		makeIfPossible(baseDirectoryPath, newRootDirectoryName);
 		addedFiles = new ArrayList<>();
 	}
 	
+	/**
+	 * Checks for the validity of the instantiation of the class.
+	 * Makes sure that the baseDirectory and new directories are valid
+	 */
 	private void makeIfPossible(String baseDirectoryPath, String newRootDirectoryName) 
 			throws InvalidRootDirectoryException, IOException{
 		rootDir = baseDirectoryPath + "/" + newRootDirectoryName;
@@ -33,32 +57,51 @@ public class NewDirectoryCreator extends AbstractFileHelper{
 		new File(rootDir).mkdirs();
 	}
 	
+	/**
+	 * returns whether or not a file with the specified name exists in the is directory
+	 */
 	@Override
 	public boolean fileExists(String filename){
-		return fileIsClean(filename) && fileCheck(rootDir + "/" + filename);
+		return fileIsClean(filename) && validFile(rootDir + "/" + filename);
 	}
 
+	/**
+	 * Adds a String file to the directory, returns whether or not the file was added (false if the file already existed)
+	 */
 	@Override
 	public boolean addStringFileToDirectory(String fileContent, String filename) throws IOException, IllegalFileException{
 		return addStringFileToDirectoryHelper(fileContent, filename, rootDir, addedFiles);
 	}
 
+	/**
+	 * Returns the String file content from the file of interest
+	 */
 	@Override
 	public String getFileContent(String filename) throws IOException, IllegalFileException{
 		return getFileContentHelper(rootDir, filename);
 	}
 
+	/**
+	 * deletes all of the file that have been added by this class
+	 */
 	@Override
 	public void cleanse() throws IOException {
 		deleteFilesHelper(addedFiles, rootDir);
 		addedFiles.clear();
 	}
 
+	/**
+	 * returns a collection of all of the new files that have been added and not yet cleared
+	 * since the object of this class was made
+	 */
 	@Override
 	public Collection<String> getAllNewFilenames() {
 		return Collections.unmodifiableCollection(addedFiles);
 	}
 
+	/**
+	 * Returns all files currently in the directory this class was instantiated to make
+	 */
 	@Override
 	public Collection<String> getAllFilesInDirectory() {
 		return Collections.unmodifiableCollection(getFilesInDir(rootDir));
